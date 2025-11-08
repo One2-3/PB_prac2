42f95b6b14fd82a1c5d8898d4e4d63fda055ba32
One2-3 <qkrwldb1695@naver.com>
2025-11-08 15:54:06 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <E12RefComponent/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import E12RefComponent from "@/components/example6/E-12-ref-component.vue";
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    E12RefComponent
+  }
+});
+</script>
+
+<style>
+</style>
---
+++ src/components/example1/E-01-instance.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>Hello, {{ message }}</div>
+</template>
+
+<script>
+export default {
+  name: "E01Instance",
+  data() {
+    const name ="JiYu"
+    return {
+      message: name
+    };
+  }
+};
+</script>
+
+<style scoped>
+div {
+  color: blue;
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
